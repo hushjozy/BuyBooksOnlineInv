651fb31d6803e5ff5dc59729ee5b72d08951ecf3
Joseph <hushjoezzybad@gmail.com>
2022-12-11 17:20:29 +0100

    c# inventory start

---
+++ BuyBooksOnlineInv/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BuyBooksOnlineInv.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
---
+++ BuyBooksOnlineInv/Models/LoginClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BuyBooksOnlineInv.Models
+{
+    public class LoginClass
+    {
+
+        public int Number { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
